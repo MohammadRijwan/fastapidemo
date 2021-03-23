88da945da8e0ddf3ff2250867302080fb902710f
MohammadRijwan <mohammad.rijwan@gmail.com>
2021-03-23 19:10:33 +0530

    init

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ app=FastAPI()
 @app.get('/')
 def root():
     return{'Status':'Welcome to the fast API'}
+
+
+@app.post('/')
+def post():
+    return{'Status':'Welcome to the fast API'}
+
+@app.put('/')
+def put():
+    return{'Status':'Welcome to the fast API'}
+
+@app.delete('/')
+def delete():
+    return{'Status':'Welcome to the fast API'}
