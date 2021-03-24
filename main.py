47964ba63ec1439ffe4f81f6c39600f9daf9ce3c
MohammadRijwan <mohammad.rijwan@gmail.com>
2021-03-24 12:12:35 +0530

    add feat note

--- main.py
+++ main.py
@@ -1,21 +1,87 @@
-from fastapi import FastAPI
+from typing import List
+import databases, sqlalchemy
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+import urllib
+import psycopg2
 
-app=FastAPI()
+
+DATABASE_URL = "postgres://rydhmtmngcyeod:2c0170103a7cae2971c9e893c5281026ac584f2f40e128815212e9874b761e8e@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d1p3fpsus6u211"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+
+metadata.create_all(engine)
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
 
 
-@app.get('/')
-def root():
-    return{'Status':'Welcome to the fast API'}
+app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
-@app.post('/')
-def post():
-    return{'Status':'Welcome to the fast API'}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
-@app.put('/')
-def put():
-    return{'Status':'Welcome to the fast API'}
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
-@app.delete('/')
-def delete():
-    return{'Status':'Welcome to the fast API'}
+
+@app.post("/notes/", response_model=Note, status_code = status.HTTP_201_CREATED)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
+
+
+@app.put("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
+async def update_note(note_id: int, payload: NoteIn):
+    query = notes.update().where(notes.c.id == note_id).values(text=payload.text, completed=payload.completed)
+    await database.execute(query)
+    return {**payload.dict(), "id": note_id}
+
+
+@app.get("/notes/", response_model=List[Note], status_code = status.HTTP_200_OK)
+async def read_notes(skip: int = 0, take: int = 20):
+    query = notes.select().offset(skip).limit(take)
+    return await database.fetch_all(query)
+
+
+@app.get("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
+async def read_notes(note_id: int):
+    query = notes.select().where(notes.c.id == note_id)
+    return await database.fetch_one(query)
+
+
+@app.delete("/notes/{note_id}/", status_code = status.HTTP_200_OK)
+async def update_note(note_id: int):
+    query = notes.delete().where(notes.c.id == note_id)
+    await database.execute(query)
+    return {"message": "Note with id: {} deleted successfully!".format(note_id)}
